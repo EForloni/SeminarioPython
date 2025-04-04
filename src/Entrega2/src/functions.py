39b252607465038f1b06cf59f40d8ce22507d3cf
Esteban <tebyforloni@gmail.com>
2025-04-04 17:27:10 -0300

    Actualizacion intermedia de entrega 2

---
+++ src/Entrega2/src/functions.py
@@ -0,0 +1,19 @@
+def initialize_player_stats():
+    #Inicializo en 0 estadisticas de jugadores
+    pass
+
+def update_player_stats(player_stats, round_data):
+    # Actualiza las estadísticas de cada jugador con los datos de la ronda
+    pass
+
+def determine_mvp(round_data):
+    #Determina el MVP de la ronda que tiene maximo puntaje
+    pass
+
+def calculate_score(kills, assists, deaths):
+    #Calcula puntaje actual
+    return (kills * 3) + (assists * 1) - (deaths * 1)
+
+def count_mvps(rounds):
+    #Cuenta cuántas veces cada jugador fue MVP
+    pass
