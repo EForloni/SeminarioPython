e47528836fefddc3e72f539eeada0e299b7c652d
Esteban <tebyforloni@gmail.com>
2025-04-04 18:11:11 -0300

    Ejercicio 10 + Functions

--- src/Entrega2/src/functions.py
+++ src/Entrega2/src/functions.py
@@ -1,19 +1,31 @@
 def initialize_player_stats():
-    #Inicializo en 0 estadisticas de jugadores
+    # Inicializo en 0 las estadísticas de cada jugador
     pass
 
 def update_player_stats(player_stats, round_data):
-    # Actualiza las estadísticas de cada jugador con los datos de la ronda
+    # Actualiza las estadísticas acumuladas de cada jugador con los datos de una ronda
     pass
 
-def determine_mvp(round_data):
-    #Determina el MVP de la ronda que tiene maximo puntaje
+def process_rounds(rounds):
+    # Procesa todas las rondas y devuelve las estadísticas finales por jugador
     pass
 
 def calculate_score(kills, assists, deaths):
-    #Calcula puntaje actual
+    # Calcula el puntaje total según kills, assists y deaths
     return (kills * 3) + (assists * 1) - (deaths * 1)
 
+def determine_mvp(round_data):
+    # Determina el MVP de una ronda (jugador con mayor puntaje)
+    pass
+
 def count_mvps(rounds):
-    #Cuenta cuántas veces cada jugador fue MVP
+    # Cuenta cuántas veces cada jugador fue MVP a lo largo de todas las rondas
+    pass
+
+def generate_round_rankings(rounds):
+    # Genera los rankings de puntajes por cada ronda
+    pass
+
+def generate_final_ranking(player_stats):
+    # Genera el ranking final acumulado de todos los jugadores
     pass
